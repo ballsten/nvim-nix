0ea29339c8559a3a8ca8467faac924fe78a68f12
Andrew Theaker <andrew@theaker.name>
2025-04-02 10:19:18 +1100

    Disable location list for now

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -105,18 +105,23 @@ map("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Commen
 --[[
 -- lazy
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+]]--
 
 -- new file
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
 -- location list
+-- TODO not sure if I need this
+--[[
 map("n", "<leader>xl", function()
   local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
   if not success and err then
     vim.notify(err, vim.log.levels.ERROR)
   end
 end, { desc = "Location List" })
+]]--
 
+--[[
 -- quickfix list
 map("n", "<leader>xq", function()
   local success, err = pcall(vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd.cclose or vim.cmd.copen)
