6ad12b614812d3b740b684ea03cd85beaaf35af0
Andrew Theaker <andrew@theaker.name>
2025-04-09 23:45:25 +0000

    enable statuscolumn

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -50,6 +50,7 @@ opt.spelllang = { "en" }
 opt.splitbelow = true -- Put new windows below current
 opt.splitkeep = "screen"
 opt.splitright = true -- Put new windows right of current
+opt.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]]
 opt.tabstop = 2 -- Number of spaces tabs count for
 opt.termguicolors = true -- True color support
 opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
@@ -113,7 +114,6 @@ opt.wrap = false -- Disable line wrap
 --
 -- opt.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
 --
--- opt.statuscolumn = [[%!v:lua.require'snacks.statuscolumn'.get()]]
 --
 -- if vim.fn.has("nvim-0.10") == 1 then
 --   opt.smoothscroll = true
