a22604c00e93f154a6d06eaba7b86677731ad3a8
Andrew Theaker <andrew@theaker.name>
2025-04-09 09:03:35 +0000

    refactor and snacks boilerplate

--- lua/plugins/core/ui.lua
+++ lua/plugins/core/ui.lua
@@ -1,5 +1,37 @@
 return {
   {
+    "lualine.nvim",
+    for_cat = "core.default",
+    event = "DeferredUIEnter",
+    -- TODO: revist after Snacks is configured, check out LazyVIM
+    after = function(plugin)
+      require('lualine').setup({
+        options = {
+          icons_enabled = false,
+          theme = nixCats('colorscheme'),
+          component_separators = '|',
+          section_separators = '',
+        },
+        sections = {
+          lualine_c = {
+            {
+              'filename', path = 1, status = true,
+            },
+          },
+        },
+        inactive_sections = {
+          lualine_b = {
+            {
+              'filename', path = 3, status = true,
+            },
+          },
+          lualine_x = {'filetype'},
+        },
+        tabline = {},
+      })
+    end,
+  },
+  {
     "bufferline.nvim",
     for_cat = "core.default",
     event = "DeferredUIEnter",
@@ -19,5 +51,15 @@ return {
       -- TODO: revisit after snacks
       require('bufferline').setup({})
     end,
+  },
+  {
+    "snacks.nvim",
+    for_cat = "core.default",
+    lazy = false,
+    cmd = { "Snacks" },
+    after = function(_)
+      require('snacks').setup({})
+    end,
   }
+
 }
