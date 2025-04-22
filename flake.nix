e6d17bfe96521c2b376aa2fa1c9796d23a1d3b42
Andrew Theaker <andrew@theaker.name>
2025-04-23 06:09:24 +1000

    update to use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,13 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in 
+      {
+        packages = rec {
+          neovim = pkgs.neovim-unwrapped;
+          default = neovim;
+        };
 
-    packages.x86_64-linux.neovim = nixpkgs.legacyPackages.x86_64-linux.neovim-unwrapped;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.neovim;
-
-  };
+        app = rec {
+          neovim = flake-utils.lib.mkApp { drv = self.packages.${system}.neovim; };
+          default = neovim;
+        };
+      });
 }
