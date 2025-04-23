6d4b92cfa5da3f77f88e82863dbece3cfae5d84e
Andrew Theaker <andrew@theaker.name>
2025-04-24 07:05:44 +1000

    loading plugins

--- neovim.nix
+++ neovim.nix
@@ -2,7 +2,25 @@
   symlinkJoin,
   neovim-unwrapped,
   makeWrapper,
-}:
+  runCommandLocal,
+  vimPlugins,
+  lib,
+}: let
+  packageName = "mypackage";
+  startPlugins = [
+    vimPlugins.lz-n
+  ];
+  packpath = runCommandLocal "packpath" {} ''
+    mkdir -p $out/pack/${packageName}/{start,opt}
+
+    ${
+      lib.concatMapStringsSep
+      "\n"
+      (plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}")
+      startPlugins
+    }
+  '';
+in 
 symlinkJoin {
   name = "neovim-custom";
   paths = [ neovim-unwrapped ];
@@ -10,6 +28,10 @@ symlinkJoin {
   postBuild = ''
     wrapProgram $out/bin/nvim \
       --add-flags '-u' \
-      --add-flags '${./init.lua}'
+      --add-flags '${./init.lua}' \
+      --add-flags '--cmd' \
+      --add-flags "'set packpath^=${packpath} | set runtimepath^=${packpath}'" \
+      --set-default NVIM_APPNAME nvim-custom
   '';
+  passthru = { inherit packpath; };
 }
