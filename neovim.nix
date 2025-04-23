3fc4701c98004224a9ec983a1cea64f6b0d630f7
Andrew Theaker <andrew@theaker.name>
2025-04-24 07:33:40 +1000

    basic framework in place

--- neovim.nix
+++ neovim.nix
@@ -6,18 +6,43 @@
   vimPlugins,
   lib,
 }: let
-  packageName = "mypackage";
-  startPlugins = [
-    vimPlugins.lz-n
+  packageName = "BallsPack";
+
+  startPlugins = with vimPlugins; [
+    lz-n
+    nvim-treesitter.withAllGrammars
   ];
+
+  optPlugins = with vimPlugins; [
+  ];
+
+  # function to resolve all dependencies
+  foldPlugins = builtins.foldl' (
+    acc: next:
+      acc
+      ++ [
+        next
+      ]
+      ++ (foldPlugins (next.dependencies or []))
+  ) [];
+
+  startPluginsWithDeps = lib.unique (foldPlugins startPlugins);
+  optPluginsWithDeps = lib.unique (foldPlugins optPlugins);
+
   packpath = runCommandLocal "packpath" {} ''
     mkdir -p $out/pack/${packageName}/{start,opt}
-
+    ln -vsfT ${./BallsVim} $out/pack/${packageName}/start/BallsVim
     ${
       lib.concatMapStringsSep
       "\n"
       (plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}")
-      startPlugins
+      startPluginsWithDeps
+    }
+    ${
+      lib.concatMapStringsSep
+      "\n"
+      (plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/opt/${lib.getName plugin}")
+      optPluginsWithDeps
     }
   '';
 in 
@@ -28,7 +53,7 @@ symlinkJoin {
   postBuild = ''
     wrapProgram $out/bin/nvim \
       --add-flags '-u' \
-      --add-flags '${./init.lua}' \
+      --add-flags 'NORC' \
       --add-flags '--cmd' \
       --add-flags "'set packpath^=${packpath} | set runtimepath^=${packpath}'" \
       --set-default NVIM_APPNAME nvim-custom
