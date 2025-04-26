6d1b85fc81d5f2c4215f31ca2a3900ac3d5b4b18
Andrew Theaker <andrew@theaker.name>
2025-04-26 10:54:37 +1000

    migrate plugins to editor

--- BallsVim/lua/specs/init.lua
+++ BallsVim/lua/specs/init.lua
@@ -3,15 +3,19 @@ return {
   { import = "specs.treesitter" },
   { import = "specs.lsp" },
   { import = "specs.completion" },
-  { import = "specs.ui.which-key" },
   { import = "specs.ui.snacks" },
   { import = "specs.ui.lualine" },
   { import = "specs.ui.bufferline" },
   { import = "specs.ui.web-devicons" },
-  { import = "specs.ui.gitsigns" },
-  { import = "specs.ui.flash" },
+
+  -- coding
   { import = "specs.coding.mini-pairs" },
   { import = "specs.coding.mini-ai" },
   { import = "specs.coding.ts-comments" },
   { import = "specs.coding.lazydev" },
+  
+  -- editor
+  { import = "specs.editor.gitsigns" },
+  { import = "specs.editor.flash" },
+  { import = "specs.editor.which-key" },
 }
