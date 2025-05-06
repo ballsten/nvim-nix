d71dcb83fc27197f7c09c0a2537377f40174933c
ballsten <andrew@theaker.name>
2025-05-07 07:12:35 +1000

    feat: remove linting

--- BallsVim/lua/specs/coding/lint.lua
+++ BallsVim/lua/specs/coding/lint.lua
@@ -4,8 +4,9 @@ return {
 	after = function(_)
 		require("lint").linters_by_ft = {
 			-- insert linters here
-			javascript = { "eslint_d", "ESLint" },
-			typescript = { "eslint_d", "ESLint" },
+			-- TODO: determine if I really want linting
+			-- javascript = { "eslint_d", "ESLint" },
+			-- typescript = { "eslint_d", "ESLint" },
 		}
 		-- create autocmd to trigger linter
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
